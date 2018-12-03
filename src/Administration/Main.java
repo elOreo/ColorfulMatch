1ab53ff148201d8bcca1a8554eebc133784cedec
Unknown <tobias.rump@gmx.de>
2018-12-03 10:31:39 +0100

    Dice

--- src/Administration/Main.java
+++ src/Administration/Main.java
@@ -1,8 +1,13 @@
 package Administration;
 
+import CG.Dice;
+
 public class Main {
 
     public static void main(String[] args){
 
+        Dice dice = new Dice();
+        int random = dice.roll();
+        System.out.println("Wuerfel: " + random);
     }
 }
--- src/CG/Dice.java
+++ src/CG/Dice.java
@@ -1,4 +1,14 @@
 package CG;
 
 public class Dice {
+
+    public Dice(){
+
+    }
+
+    public int roll(){
+        int random;
+        random = (int) Math.random()*6;
+        return random;
+    }
 }
